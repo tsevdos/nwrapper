94e5bc06764b80893fff80703422b196d0840fb8
John Tsevdos <tsevdosjohn@googlemail.com>
2011-06-27 14:45:34 +0300

    ver1
    git status
    git commit -m ver1

---
+++ jquery.nwrapper.js
@@ -0,0 +1,56 @@
+﻿(function($) {
+
+$.fn.nwrapper = function(options){
+
+	var defaults = {
+		wrapEvery      : 1,
+		defaultClasses : true,
+		extraClasses   : false,
+		htmlStructure  : 'div'
+    };
+
+	settings = $.extend({}, defaults, options);
+
+	var elements    = $(this).children();
+	var elementsLen = elements.length;
+
+	for ( var i = 0, numb = 1; i < elementsLen; i += settings.wrapEvery, numb++ ){
+		
+		// Default Classes Array
+		var classes = [];
+		if ( settings.defaultClasses ) {
+		
+			classes[0] = 'wrapper';
+			classes[1] = 'wrapper-' + numb;
+
+			if (numb==1) {
+				classes[2] = 'first';
+			}
+				
+
+			if (numb==Math.ceil(elementsLen/settings.wrapEvery)) {
+				classes[2] = 'last';
+			}
+
+		}
+
+		// Merge Default class with Extra Class
+		if ( settings.extraClasses ) {
+			$.merge( classes, settings.extraClasses );
+		}
+		
+		// If you find any classes crete the class string
+		if ( classes.length > 0 ) {
+			htmlClassesString = 'class="' + classes.join(" ") + '"';
+		} else {
+			htmlClassesString = '';
+		}
+		
+		elements.filter(':eq(' + i + '), :lt(' + (i + settings.wrapEvery) + '):gt(' + i + ')').wrapAll('<' + settings.htmlStructure + ' ' + htmlClassesString + ' />');
+
+	}
+	
+	return $(this);
+};
+
+})(jQuery);
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Nwrapper jQuery plugin
+----------------------
+
+Nwrapper is very similar to jQuery's core .wrap() function, but allows you to wrap n number of elements! For more information and code explanation have a look at my site's blog post "Wrap any amount of elements with jQuery" http://phrappe.com/javascript/wrap-any-amount-of-elements-with-jquery/ or have a look at the demo page http://phrappe.com/demos/nwrapper/nwrapper.html
+
+Keep in mind that nwrapper jQuery plugin is also hosted at the official jQuery repository http://plugins.jquery.com/project/nwrapper
